970846c9b5fdb8143f986e5e82d97fe4e06fcc95
jakub-vesely <jakub_vesely@seznam.cz>
2015-11-07 13:01:26 +0100

    translated missed strings

--- SerialToGraph/languages/serialToGraph_cs.ts
+++ SerialToGraph/languages/serialToGraph_cs.ts
@@ -4,53 +4,78 @@
 <context>
     <name>ButtonLine</name>
     <message>
-        <location filename="../ButtonLine.cpp" line="30"/>
+        <location filename="../ButtonLine.cpp" line="38"/>
+        <source>Menu</source>
+        <translation>Nabídka</translation>
+    </message>
+    <message>
+        <location filename="../ButtonLine.cpp" line="43"/>
         <source>Frequency</source>
         <translation>Frekvence</translation>
     </message>
     <message>
-        <location filename="../ButtonLine.cpp" line="31"/>
+        <location filename="../ButtonLine.cpp" line="44"/>
         <source>Time</source>
         <translation>Čas</translation>
     </message>
     <message>
-        <location filename="../ButtonLine.cpp" line="46"/>
-        <location filename="../ButtonLine.cpp" line="114"/>
+        <location filename="../ButtonLine.cpp" line="59"/>
+        <location filename="../ButtonLine.cpp" line="235"/>
         <source>Hz</source>
         <translation>Hz</translation>
     </message>
     <message>
-        <location filename="../ButtonLine.cpp" line="50"/>
+        <location filename="../ButtonLine.cpp" line="63"/>
         <source>Start</source>
         <translation>Start</translation>
     </message>
     <message>
-        <location filename="../ButtonLine.cpp" line="58"/>
+        <location filename="../ButtonLine.cpp" line="71"/>
         <source>Stop</source>
         <translation>Stop</translation>
     </message>
     <message>
-        <location filename="../ButtonLine.cpp" line="64"/>
+        <location filename="../ButtonLine.cpp" line="123"/>
         <source>Export to PNG</source>
         <translation>Exportovat do PNG</translation>
     </message>
     <message>
-        <location filename="../ButtonLine.cpp" line="68"/>
+        <location filename="../ButtonLine.cpp" line="124"/>
         <source>Export to CSV</source>
         <translation>Exportovat do CSV</translation>
     </message>
     <message>
-        <location filename="../ButtonLine.cpp" line="114"/>
+        <location filename="../ButtonLine.cpp" line="127"/>
+        <source>Panels</source>
+        <translation>Panely</translation>
+    </message>
+    <message>
+        <location filename="../ButtonLine.cpp" line="130"/>
+        <source>Graph</source>
+        <translation>Graf</translation>
+    </message>
+    <message>
+        <location filename="../ButtonLine.cpp" line="133"/>
+        <source>Show All</source>
+        <translation>Zobrazit vše</translation>
+    </message>
+    <message>
+        <location filename="../ButtonLine.cpp" line="134"/>
+        <source>Show None</source>
+        <translation>Skrýt vše</translation>
+    </message>
+    <message>
+        <location filename="../ButtonLine.cpp" line="235"/>
         <source>s</source>
         <translation>s</translation>
     </message>
     <message>
-        <location filename="../ButtonLine.cpp" line="149"/>
+        <location filename="../ButtonLine.cpp" line="270"/>
         <source>Connected</source>
         <translation>Připojeno</translation>
     </message>
     <message>
-        <location filename="../ButtonLine.cpp" line="154"/>
+        <location filename="../ButtonLine.cpp" line="275"/>
         <source>Disconnected</source>
         <translation>Odpojeno</translation>
     </message>
@@ -58,12 +83,10 @@
 <context>
     <name>Channel</name>
     <message>
-        <location filename="../Channel.cpp" line="67"/>
         <source>n/a</source>
         <translation>n/a</translation>
     </message>
     <message>
-        <location filename="../Channel.cpp" line="82"/>
         <source>hidden</source>
         <translation>skrytý</translation>
     </message>
@@ -71,112 +94,111 @@
 <context>
     <name>ChannelSettings</name>
     <message>
-        <location filename="../ChannelSettings.cpp" line="27"/>
+        <location filename="../ChannelSettings.cpp" line="19"/>
         <source>Channel settings</source>
         <translation>Nastavení kanálu</translation>
     </message>
     <message>
-        <location filename="../ChannelSettings.cpp" line="44"/>
+        <location filename="../ChannelSettings.cpp" line="26"/>
         <source>Title</source>
         <translation>Název</translation>
     </message>
     <message>
-        <location filename="../ChannelSettings.cpp" line="36"/>
         <source>Selected</source>
         <translation>Vybraný</translation>
     </message>
     <message>
-        <location filename="../ChannelSettings.cpp" line="51"/>
+        <location filename="../ChannelSettings.cpp" line="33"/>
         <source>Units</source>
         <translation>Jednotky</translation>
     </message>
     <message>
-        <location filename="../ChannelSettings.cpp" line="56"/>
+        <location filename="../ChannelSettings.cpp" line="38"/>
         <source>To right side</source>
         <translation>Na pravou stranu</translation>
     </message>
     <message>
-        <location filename="../ChannelSettings.cpp" line="68"/>
+        <location filename="../ChannelSettings.cpp" line="50"/>
         <source>Store</source>
         <translation>Ulož</translation>
     </message>
     <message>
-        <location filename="../ChannelSettings.cpp" line="72"/>
+        <location filename="../ChannelSettings.cpp" line="54"/>
         <source>Cancel</source>
         <translation>Zruš</translation>
     </message>
     <message>
-        <location filename="../ChannelSettings.cpp" line="85"/>
+        <location filename="../ChannelSettings.cpp" line="94"/>
         <source>Shape</source>
         <translation>Tvar</translation>
     </message>
     <message>
-        <location filename="../ChannelSettings.cpp" line="90"/>
+        <location filename="../ChannelSettings.cpp" line="99"/>
         <source>Cross</source>
         <translation>Kříž</translation>
     </message>
     <message>
-        <location filename="../ChannelSettings.cpp" line="91"/>
+        <location filename="../ChannelSettings.cpp" line="100"/>
         <source>Plus</source>
         <translation>Plus</translation>
     </message>
     <message>
-        <location filename="../ChannelSettings.cpp" line="92"/>
+        <location filename="../ChannelSettings.cpp" line="101"/>
         <source>Circle</source>
         <translation>Krouh</translation>
     </message>
     <message>
-        <location filename="../ChannelSettings.cpp" line="93"/>
+        <location filename="../ChannelSettings.cpp" line="102"/>
         <source>Disc</source>
         <translation>Kružnice</translation>
     </message>
     <message>
-        <location filename="../ChannelSettings.cpp" line="94"/>
+        <location filename="../ChannelSettings.cpp" line="103"/>
         <source>Square</source>
         <translation>Čtverec</translation>
     </message>
     <message>
-        <location filename="../ChannelSettings.cpp" line="95"/>
+        <location filename="../ChannelSettings.cpp" line="104"/>
         <source>Diamond</source>
         <translation>Diamant</translation>
     </message>
     <message>
-        <location filename="../ChannelSettings.cpp" line="96"/>
+        <location filename="../ChannelSettings.cpp" line="105"/>
         <source>Star</source>
         <translation>Hvězda</translation>
     </message>
     <message>
-        <location filename="../ChannelSettings.cpp" line="97"/>
+        <location filename="../ChannelSettings.cpp" line="106"/>
         <source>Triangle</source>
         <translation>Trojúhelník</translation>
     </message>
     <message>
-        <location filename="../ChannelSettings.cpp" line="98"/>
+        <location filename="../ChannelSettings.cpp" line="107"/>
         <source>Inverted Triangle</source>
         <translation>Obrácený trojúhelník</translation>
     </message>
     <message>
-        <location filename="../ChannelSettings.cpp" line="99"/>
+        <location filename="../ChannelSettings.cpp" line="108"/>
         <source>Cross and Square</source>
         <translation>Kříž a Čtverec</translation>
     </message>
     <message>
-        <location filename="../ChannelSettings.cpp" line="100"/>
+        <location filename="../ChannelSettings.cpp" line="109"/>
         <source>Plus and Square</source>
         <translation>Plus a Čtverec</translation>
     </message>
     <message>
-        <location filename="../ChannelSettings.cpp" line="101"/>
+        <location filename="../ChannelSettings.cpp" line="110"/>
         <source>Cross and Circle</source>
         <translation>Kříž a Kruh</translation>
     </message>
     <message>
-        <location filename="../ChannelSettings.cpp" line="102"/>
+        <location filename="../ChannelSettings.cpp" line="111"/>
         <source>Plus and Circle</source>
         <translation>Plus a Kruh</translation>
     </message>
     <message>
-        <location filename="../ChannelSettings.cpp" line="103"/>
+        <location filename="../ChannelSettings.cpp" line="112"/>
         <source>Peace</source>
         <translation>Mír</translation>
     </message>
@@ -184,12 +206,31 @@
 <context>
     <name>ChannelSideBar</name>
     <message>
-        <location filename="../ChannelSideBar.cpp" line="27"/>
         <source>sample</source>
         <translation>vzorek</translation>
     </message>
     <message>
-        <location filename="../ChannelSideBar.cpp" line="50"/>
+        <source>channel %1</source>
+        <translation>kanál %1</translation>
+    </message>
+</context>
+<context>
+    <name>DisplayWidget</name>
+    <message>
+        <location filename="../DisplayWidget.cpp" line="126"/>
+        <source>n/a</source>
+        <translation>n/a</translation>
+    </message>
+</context>
+<context>
+    <name>Graph</name>
+    <message>
+        <location filename="../Graph.cpp" line="572"/>
+        <source>samples</source>
+        <translation>vzorky</translation>
+    </message>
+    <message>
+        <location filename="../Graph.cpp" line="594"/>
         <source>channel %1</source>
         <translation>kanál %1</translation>
     </message>

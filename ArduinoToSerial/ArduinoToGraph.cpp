4cbf41acfd346319afc9dce8b99b2f8e6067e9f3
jakub-vesely <jakub_vesely@seznam.cz>
2016-01-01 13:25:15 +0100

    fix of bug: first values was not transferred immediately

--- ArduinoToSerial/ArduinoToGraph.cpp
+++ ArduinoToSerial/ArduinoToGraph.cpp
@@ -68,15 +68,20 @@ namespace
   }
 }
 
+void ProcessChannels()
+{
+  g_updateFunction();  
+  SendData();
+}
+
 ISR(TIMER1_COMPA_vect)          // timer compare interrupt service routine
 {
-   g_updateFunction();
    if (0 != g_requiredTime && (++g_currentTime) != g_requiredTime)
     return;
 
-  g_currentTime = 0;
+   g_currentTime = 0;
   
-  SendData();
+   ProcessChannels();
 }
 
 void ArtuinoToGraph::Setup(float channel1, float channel2, float channel3, float channel4, float channel5, float channel6, float channel7, float channel8)
@@ -148,6 +153,7 @@ void ArtuinoToGraph::Loop()
       g_currentTime = 0;
       g_fullWriteBufferDetected = false;
       TIMSK1 |= (1 << OCIE1A);  // enable timer compare interrupt
+      ProcessChannels();
     break;
     case INS_STOP:
       TIMSK1 &= ~(1 << OCIE1A);  // disable timer compare interrupt

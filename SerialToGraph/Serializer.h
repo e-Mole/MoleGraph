de0dc1582349b1de65423c1140998592a19b41cc
Jakub Veselý <jakub_vesely@seznam.cz>
2016-03-13 08:22:44 +0100

    changed .atog file version. Now is stored ID to be able add properties to objects

--- SerialToGraph/Serializer.h
+++ SerialToGraph/Serializer.h
@@ -2,13 +2,21 @@
 #define SERIALIZER_H
 
 #include <QDataStream>
+#include <QDebug>
 #include <QMetaObject>
 #include <QMetaProperty>
 #include <QString>
 #include <QVariant>
 #include <QDebug>
 
-#define ATOG_SERIALIZER_VERSION 1
+namespace atog
+{
+
+#define ATOG_SERIALIZER_VERSION 2
+#define TERMINATOR_STRING "!end!"
+#define COLECTIONS_ID "colections"
+
+static unsigned GetSerializerVersion() {return ATOG_SERIALIZER_VERSION;}
 
 template <class T>
 QDataStream &operator<<(QDataStream &out, T *t)
@@ -17,35 +25,44 @@ QDataStream &operator<<(QDataStream &out, T *t)
     {
         if(t->metaObject()->property(i).isStored(t))
         {
+            out << QString(t->metaObject()->property(i).name());
             //I was not patient to search how to serialize collections like axis or channels so I do it manually
-            if (t->metaObject()->property(i).name() == QString("colections"))
+            if (t->metaObject()->property(i).name() == QString(COLECTIONS_ID))
                 t->SerializeColections(out);
             else
             out << t->metaObject()->property(i).read(t);
-
         }
     }
+    out << QString(TERMINATOR_STRING);
+           
     return out;
 }
 
 template <class T>
 QDataStream &operator>>(QDataStream &in, T *t)
 {
-    QVariant var;
-    for(int i=0; i<t->metaObject()->propertyCount(); ++i)
+    while (true)
     {
-        if(t->metaObject()->property(i).isStored(t))
+        QString propertyName;
+        in >> propertyName;
+        if (propertyName == TERMINATOR_STRING)
+            return in;
+
+        if (propertyName == COLECTIONS_ID)
+            t->DeserializeColections(in);
+        else
         {
-            if (t->metaObject()->property(i).name() == QString("colections"))
-                t->DeserializeColections(in);
-            else
-            {
-                in >> var;
-                t->metaObject()->property(i).write(t, var);
-            }
+            QVariant var;
+            in >> var;
+
+            int index = t->metaObject()->indexOfProperty(propertyName.toStdString().c_str());
+            if (index != -1)
+                t->metaObject()->property(index).write(t, var);
         }
     }
+
     return in;
 }
 
+} //namespace atog
 #endif // SERIALIZER_H

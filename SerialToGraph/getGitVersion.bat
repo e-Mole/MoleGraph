a8abbacaf1e41b922b7a3703fc41cb84530efb81
jakub-vesely <jakub_vesely@seznam.cz>
2016-06-11 15:07:33 +0200

    joining .bad and .sh file together (to .bat) once again from linux (previous commit was not copilable on linux)

--- SerialToGraph/getGitVersion.bat
+++ SerialToGraph/getGitVersion.bat
@@ -1,3 +1,14 @@
+:;cd $1
+:;filename=GitVersion.h
+:;echo '//generated file - do not edit' > $filename
+:;echo '#ifndef VERSION_H' >> $filename
+:;echo '#define VERSION_H' >> $filename
+:;version=$(git describe --always --tags --dirty)
+:;echo '#	define GIT_VERSION "'$version'"' >> $filename
+:;echo '#endif //VERSION_H' >> $filename
+:;echo git version: $version
+:;exit
+
 @echo off
 cd %1
 set filename= GitVersion.h
@@ -7,3 +18,4 @@ for /f "delims=" %%i in ('git describe --always --tags --dirty') do set version=
 echo #	define GIT_VERSION "%version%" >> %filename%
 echo #endif //VERSION_H >> %filename%
 echo git version: %version% 
+

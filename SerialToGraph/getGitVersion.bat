851e731d8676d579ba87425ecd877e735ccf6f84
Jakub Veselý <jakub_vesely@seznam.cz>
2016-06-11 14:18:03 +0200

    joining .bad and .sh file together (to .bat) I want to build android on linux as same as on windows too. I'm no table to distinguish unned code in .pro file according a current platform. so I joined both of files together.

--- SerialToGraph/getGitVersion.bat
+++ SerialToGraph/getGitVersion.bat
@@ -1,3 +1,15 @@
+:;#in this file is bash and batch code together because I want to be able build android version of the MoleGraph on linux as same on windows 
+:;cd $1
+:;filename=GitVersion.h
+:;echo '//generated file - do not edit' > $filename
+:;echo '#ifndef VERSION_H' >> $filename
+:;echo '#define VERSION_H' >> $filename
+:;version=$(git describe --always --tags --dirty)
+:;echo '#	define GIT_VERSION "'$version'"' >> $filename
+:;echo '#endif //VERSION_H' >> $filename
+:;echo git version: $version
+:;exit
+
 @echo off
 cd %1
 set filename= GitVersion.h

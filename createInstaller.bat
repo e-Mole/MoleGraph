c4870845b099b02affca8d39a553c44e207092e1
Jakub Veselý <jakub_vesely@seznam.cz>
2016-07-04 15:18:24 +0200

    added Arduino to instlation data

--- createInstaller.bat
+++ createInstaller.bat
@@ -5,7 +5,7 @@
 ::arguments:
 ::  as argument is expected a folder name where builded .exe file is
 ::installer is created in argument directory in subir 'installer'
-
+ 
 set curDir=%cd%
 rmdir %1\installer /q /s
 mkdir %1\installer
@@ -14,4 +14,20 @@ cd %1\installer
 copy ..\MoleGraph.exe packages\cz.emole\data\MoleGraph.exe
 windeployqt --no-quick-import --no-translations --no-system-d3d-compiler --no-webkit2 --no-angle --no-opengl-sw packages\cz.emole\data\
 binarycreator --offline-only -c config/config.xml -p packages MoleGraph-Installer
+
 cd %curdir%
+
+mkdir %1\installer\Arduino
+mkdir %1\installer\Arduino\MoleGraph
+copy Arduino\MoleGraph\keywords.txt %1\installer\Arduino\MoleGraph
+copy Arduino\MoleGraph\MoleGraph.cpp %1\installer\Arduino\MoleGraph
+copy Arduino\MoleGraph\MoleGraph.h %1\installer\Arduino\MoleGraph
+
+mkdir %1\installer\Arduino\MoleGraph\examples
+xcopy /S  Arduino\MoleGraph\examples  %1\installer\Arduino\MoleGraph\examples
+
+mkdir %1\installer\Arduino\MoleGraph\examples\signalEmulation
+copy Arduino\MoleGraph\MoleGraph.ino  %1\installer\Arduino\MoleGraph\examples\signalEmulation\signalEmulation.ino
+
+
+

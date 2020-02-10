039d563e31771f906ed232a01d8fb60711f54faa
Tomas Feltl <casopis.e.mole@gmail.com>
2020-02-10 18:20:44 +0100

    Added sensors: - Simple heart pulse sensor (PulseSensor Playground sensor) - Thermometer (K-Type Thermocouple + MAX6675) New translations Update sensors.json Change ButtonLine.cpp - reenable "Recent files" menu

---
+++ Arduino/MoleGraphAuto/pulse.h
@@ -0,0 +1,23 @@
+#ifndef PULSE_h
+#define PULSE_h
+
+#include <Arduino.h>
+#include "systick.h"
+#include "Sensor.h"
+
+class PULSE : public Sensor {
+  public:
+    PULSE(uint32_t, uint8_t);
+    virtual bool process();
+    virtual float read(uint8_t);
+  protected:
+    uint8_t   pin;
+  private:
+    unsigned int value2;
+    unsigned int BPM;
+    unsigned long bpmRead[5] = {0,0,0,0,0};
+    bool counted = false;	
+};
+
+
+#endif

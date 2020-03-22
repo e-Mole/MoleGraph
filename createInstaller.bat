46e5ab35b99daaff890e00360f11826199347d9c
Jakub Vesely <jakub_vesely@seznam.cz>
2020-03-22 11:51:47 +0100

    added sensors.json to installer package + fixed extension file text

--- createInstaller.bat
+++ createInstaller.bat
@@ -1,18 +1,22 @@
 ::script gethers .exe file dependencies and creates an installer
-::prerequisities: 
+::prerequisities:
 ::  * path to QtIFW bin directory placed in a system path
 ::  * script is run from a qt's mingw command line
 ::arguments:
 ::  as argument is expected a folder name where builded .exe file is
 ::installer is created in argument directory in subir 'installer'
- 
+
 set curDir=%cd%
 rmdir %1\installer /q /s
 mkdir %1\installer
-xcopy /E Installer\* %1\installer 
+xcopy /E Installer\* %1\installer
 cd %1\installer
 mkdir packages\cz.emole\data
 copy ..\MoleGraph.exe packages\cz.emole\data\MoleGraph.exe
+
+mkdir packages\cz.emole\data\sensors
+xcopy /S %curDir%\SerialToGraph\sensors packages\cz.emole\data\sensors
+
 windeployqt --no-quick-import --no-translations --no-system-d3d-compiler --no-webkit2 --no-angle --no-opengl-sw packages\cz.emole\data\
 binarycreator --offline-only -c config/config.xml -p packages MoleGraph-Installer
 

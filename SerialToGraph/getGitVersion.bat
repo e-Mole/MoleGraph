b16ad36ed81db247105d54ce55d7fbcf5b93e5ce
Jakub Vesely <jakub_vesely@seznam.cz>
2024-03-17 13:27:44 -0700

    bugfix: multiple times simulatously executed gitGetVersion.bat for the ancroid build

--- SerialToGraph/getGitVersion.bat
+++ SerialToGraph/getGitVersion.bat
@@ -11,11 +11,19 @@
 
 @echo off
 cd %1
-set filename= GitVersion.h
-echo #ifndef VERSION_H > %filename%
-echo #define VERSION_H >> %filename%
+
 for /f "delims=" %%i in ('git describe --always --tags --dirty') do set version=%%i
+set filename= GitVersion.h
+
+if not exist ".lock" (
+echo.> ".lock"
+del %filename%
+echo #ifndef VERSION_H > %filename%
+echo #define VERSION_H >> %filename% 
 echo #define GIT_VERSION "%version%" >> %filename%
 echo #endif //VERSION_H >> %filename%
 echo git version: %version% 
+ del ".lock"
+) 
+
 
